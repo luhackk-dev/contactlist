1f1df69640e910e86af9de00098b00d0a83e03c5
luhackk <luhack@gmail.com>
2025-05-14 15:09:17 -0300

    add on main fuction Create

---
+++ main.py
@@ -0,0 +1,12 @@
+import create
+
+CREATE_OPTION = "3"
+
+if option_crud == CREATE_OPTION:
+    
+    name = input("Type the name contact: ")
+    email = input("Type the email contact: ")
+    phone = input("Type the phone: ")
+
+    create.create_contact(name, email, phone)
+    
