749843b7a3bb9e98abe3fb84786aaab8042f8b2f
luhackk <luhack@gmail.com>
2025-05-14 09:22:58 -0300

    Option Create built on main

---
+++ main.py
@@ -0,0 +1,9 @@
+import create
+
+name = input("Type the name of your contact:")
+email = input("Type the email of your contact: ")
+phone = input("Type the phone of your contact: ")
+
+if option == "3":
+    create.create_contact(name, email, phone)
+       
