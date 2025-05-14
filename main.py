95239fc1946a6dba0ecdbe1d504a638ebb0493ed
luhackk <luhack@gmail.com>
2025-05-14 09:27:03 -0300

    correct logic bug

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import create
 
-name = input("Type the name of your contact:")
-email = input("Type the email of your contact: ")
-phone = input("Type the phone of your contact: ")
-
 if option == "3":
+
+    name = input("Type the name of your contact:")
+    email = input("Type the email of your contact: ")
+    phone = input("Type the phone of your contact: ")
+
     create.create_contact(name, email, phone)
        
