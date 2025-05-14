0019d205be93e94df40cf2026e60cbb2c8b95300
luhackk <luhack@gmail.com>
2025-05-14 15:29:49 -0300

    change from write to append

--- create.py
+++ create.py
@@ -2,7 +2,7 @@ import enviroment
 
 def create_contact(name: str, email: str, phone: str):
 
-    with open(enviroment.DATABASE_NAME, "w") as file:
+    with open(enviroment.DATABASE_NAME, "a") as file:
 
         line = f"Name: {name} Email: {email} Phone: {phone}"
         
