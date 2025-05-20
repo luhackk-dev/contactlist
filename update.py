c56202299dc27bb7d025d0bada64502fb3807a90
luhackk <luhack@gmail.com>
2025-05-20 17:59:37 -0300

    fix function update

--- update.py
+++ update.py
@@ -1,17 +1,28 @@
 import environment
 
-def collect_info(name, email, phone):
-    name = input("Type the name contact: ")
-    email = input("Type the email contact: ")
-    phone = input("Type the phone: ")
+def collect_info():
+    name = input("Type the new name contact: ")
+    email = input("Type the new email contact: ")
+    phone = input("Type the new phone: ")
 
     string_contact = f"Name: {name} Email: {email} Phone: {phone}\n"
 
     return string_contact
 
-def update_contact():
+def update_contact(finder: str):
 
-    with open(environment.DATABASE_NAME, "r+") as file:
-        old = file.read()
+    with open(environment.DATABASE_NAME, "r") as file:
+       
+        lines = file.readlines()
+        line_position = 0
 
-    file.write(collect_info() + old)
+        for line in lines:
+            if finder in line:
+                lines[line_position] = collect_info()
+                break
+            
+            line_position += 1
+            
+    
+    with open(environment.DATABASE_NAME, "w") as file:
+        file.writelines(lines)
