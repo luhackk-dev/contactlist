0312cff1af3ead3b7af816b4dc4ae899a81b449b
luhackk <luhack@gmail.com>
2025-05-20 16:45:40 -0300

    add function update contact

--- update.py
+++ update.py
@@ -8,3 +8,10 @@ def collect_info(name, email, phone):
     string_contact = f"Name: {name} Email: {email} Phone: {phone}\n"
 
     return string_contact
+
+def update_contact(finder):
+
+    with open(environment.DATABASE_NAME, "r+") as file:
+        old = file.read()
+
+    file.write(collect_info() + old)
