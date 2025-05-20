b4fa084e5d3806d0f400b1db5003a9d0a8941034
luhackk <luhack@gmail.com>
2025-05-20 18:23:24 -0300

    add new file info

---
+++ info.py
@@ -0,0 +1,6 @@
+def collect_info():
+    name = input("Type the new name contact: ")
+    email = input("Type the new email contact: ")
+    phone = input("Type the new phone: ")
+
+    return f"Name: {name} Email: {email} Phone: {phone}\n"
